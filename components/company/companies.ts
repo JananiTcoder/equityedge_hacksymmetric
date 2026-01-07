1694403b39b6ca4ab32c4eb6d537be8f70afaf70
Janani T <jananiroshini2005@gmail.com>
2026-01-07 06:47:52 +0530

    Add new companies to the companies list

--- components/company/companies.ts
+++ components/company/companies.ts
@@ -1,14 +1,11 @@
 import { Company } from "@/types";
 
 export const companies: Company[] = [
-  { id: "tcs", name: "Tata Consultancy Services", ticker: "TCS", exchange: "NSE", sector: "Technology", industry: "IT Services", logo: "tcs.png" },
-  { id: "infosys", name: "Infosys", ticker: "INFY", exchange: "NSE", sector: "Technology", industry: "IT Services", logo: "infosys.png" },
-  { id: "wipro", name: "Wipro", ticker: "WIPRO", exchange: "NSE", sector: "Technology", industry: "IT Services", logo: "wipro.png" },
-  { id: "hcl", name: "HCL Technologies", ticker: "HCLTECH", exchange: "NSE", sector: "Technology", industry: "IT Services", logo: "hcl.png" },
-  { id: "hdfc", name: "HDFC Bank", ticker: "HDFCBANK", exchange: "NSE", sector: "Banking", industry: "Private Bank", logo: "hdfc.png" },
-  { id: "icici", name: "ICICI Bank", ticker: "ICICIBANK", exchange: "NSE", sector: "Banking", industry: "Private Bank", logo: "icici.png" },
-  { id: "sbi", name: "State Bank of India", ticker: "SBIN", exchange: "NSE", sector: "Banking", industry: "Public Bank", logo: "sbi.png" },
-  { id: "kotak", name: "Kotak Mahindra Bank", ticker: "KOTAKBANK", exchange: "NSE", sector: "Banking", industry: "Private Bank", logo: "kotak.png" },
-  { id: "reliance", name: "Reliance Industries", ticker: "RELIANCE", exchange: "NSE", sector: "Oil & Gas", industry: "Integrated Oil & Gas", logo: "reliance.png" },
-  { id: "ongc", name: "Oil and Natural Gas Corporation", ticker: "ONGC", exchange: "NSE", sector: "Oil & Gas", industry: "Oil Exploration", logo: "ongc.png" },
+  { id: "aapl", name: "Apple", ticker: "AAPL", exchange: "NSE", sector: "Technology", industry: "Consumer Electronics", logo: "aapl.png" },
+  { id: "googl", name: "Google", ticker: "GOOGL", exchange: "NSE", sector: "Technology", industry: "Internet Services", logo: "googl.png" },
+  { id: "msft", name: "Microsoft", ticker: "MSFT", exchange: "NSE", sector: "Technology", industry: "Software", logo: "msft.png" },
+  { id: "amzn", name: "Amazon", ticker: "AMZN", exchange: "NSE", sector: "E-commerce", industry: "Retail", logo: "amzn.png" },
+  { id: "tsla", name: "Tesla", ticker: "TSLA", exchange: "NSE", sector: "Automotive", industry: "Electric Vehicles", logo: "tsla.png" },
+  { id: "meta", name: "Meta", ticker: "META", exchange: "NSE", sector: "Technology", industry: "Social Media", logo: "meta.png" },
+  { id: "nvda", name: "Nvidia", ticker: "NVDA", exchange: "NSE", sector: "Technology", industry: "Semiconductors", logo: "nvda.png" },
 ];
