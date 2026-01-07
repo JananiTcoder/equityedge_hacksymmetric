cfd0b80a77900045332ed82a9d38579f82099470
Janani T <jananiroshini2005@gmail.com>
2026-01-07 06:43:15 +0530

    Add companies data with details and logos

---
+++ components/company/companies.ts
@@ -0,0 +1,14 @@
+import { Company } from "@/types";
+
+export const companies: Company[] = [
+  { id: "tcs", name: "Tata Consultancy Services", ticker: "TCS", exchange: "NSE", sector: "Technology", industry: "IT Services", logo: "tcs.png" },
+  { id: "infosys", name: "Infosys", ticker: "INFY", exchange: "NSE", sector: "Technology", industry: "IT Services", logo: "infosys.png" },
+  { id: "wipro", name: "Wipro", ticker: "WIPRO", exchange: "NSE", sector: "Technology", industry: "IT Services", logo: "wipro.png" },
+  { id: "hcl", name: "HCL Technologies", ticker: "HCLTECH", exchange: "NSE", sector: "Technology", industry: "IT Services", logo: "hcl.png" },
+  { id: "hdfc", name: "HDFC Bank", ticker: "HDFCBANK", exchange: "NSE", sector: "Banking", industry: "Private Bank", logo: "hdfc.png" },
+  { id: "icici", name: "ICICI Bank", ticker: "ICICIBANK", exchange: "NSE", sector: "Banking", industry: "Private Bank", logo: "icici.png" },
+  { id: "sbi", name: "State Bank of India", ticker: "SBIN", exchange: "NSE", sector: "Banking", industry: "Public Bank", logo: "sbi.png" },
+  { id: "kotak", name: "Kotak Mahindra Bank", ticker: "KOTAKBANK", exchange: "NSE", sector: "Banking", industry: "Private Bank", logo: "kotak.png" },
+  { id: "reliance", name: "Reliance Industries", ticker: "RELIANCE", exchange: "NSE", sector: "Oil & Gas", industry: "Integrated Oil & Gas", logo: "reliance.png" },
+  { id: "ongc", name: "Oil and Natural Gas Corporation", ticker: "ONGC", exchange: "NSE", sector: "Oil & Gas", industry: "Oil Exploration", logo: "ongc.png" },
+];
